e584d55f2af719dbfa9676ff983af4c0b30d09ec
Guga <g.juma.tm@gmail.com>
2021-09-04 09:59:09 +0800

    after removing Counter component and adding buttons instead

--- src/components/UI/CartItem.js
+++ src/components/UI/CartItem.js
@@ -1,7 +1,28 @@
-import CountForm from '../Meals/CountForm'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart-context'
 import styles from './Card.module.css'
 
 const Card = (props) => {
+    const { addItem, removeItem } = useContext(CartContext)
+
+    let amount = props.item.amount
+    const addOne = () => {
+        amount += 1;
+        addItem({
+            id: props.item.id,
+            name: props.item.name,
+            price: props.item.price,
+            img: props.item.img,
+            amount: amount
+        })
+    }
+
+    const amountHandler = (amount) => {
+
+    }
+
+    // const removeItemFromCart = removeItem(props.id)
+
     return (
         <div className={styles["card"]}>
             <div className={styles["card-img"]}>
@@ -12,7 +33,11 @@ const Card = (props) => {
                     <h3>{props.item.name}</h3>
                     <h3>{props.item.price}</h3>
                 </div>
-                <CountForm amount={props.item.amount} />
+                <div className={styles["cart-item-counter-container"]}>
+                    <button className={styles["cart-item-counter-button"]}> -</button>
+                    <h3>{props.item.amount}</h3>
+                    <button className={styles["cart-item-counter-button"]} onClick={addOne}> +</button>
+                </div>
             </div>
         </div>
     );
